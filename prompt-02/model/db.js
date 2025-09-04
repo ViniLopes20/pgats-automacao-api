c11c3dc0a2b25265a6f4656ddc04f4b48b33920b
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-09-04 01:55:52 -0300

    feat: initialize users array with default admin user

--- prompt-02/model/db.js
+++ prompt-02/model/db.js
@@ -1,6 +1,13 @@
 // Banco de dados em memória
 const db = {
-  users: [],
+  users: [
+    {
+      "username": "admin",
+      "password": "12345678",
+      "saldo": 1000,
+      "favorecido": true
+    }
+  ],
   transfers: []
 };
 
